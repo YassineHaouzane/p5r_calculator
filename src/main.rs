82da71dd1c956594cc293483ea3e9946dbb6702c
Yassine Haouzane <haouzaneyassine@gmail.com>
2023-03-20 12:21:06 +0100

    feat(cors): adding cors for testing purposes

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod controller;
 mod data_definitions;
 mod data_helper;
 mod royal_data;
+use actix_cors::Cors;
 use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
 use controller::{get_personas, get_recipes_of_persona};
@@ -16,8 +17,12 @@ async fn main() -> std::io::Result<()> {
     info!("Starting server on port 8080");
 
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:5173")
+            .allowed_methods(vec!["GET"]);
         App::new()
             .wrap(Logger::default())
+            .wrap(cors)
             .app_data(build_global_data())
             .service(get_personas)
             .service(get_recipes_of_persona)
